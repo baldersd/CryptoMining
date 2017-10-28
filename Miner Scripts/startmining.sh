7eb979d79f5050d6f7227be5c0b919312cb65738
baldersd <danb@swiftdev.net>
2017-10-28 16:36:07 +0100

    Update startmining.sh

--- Miner Scripts/startmining.sh
+++ Miner Scripts/startmining.sh
@@ -2,12 +2,13 @@
 PATH=/home/dbaldree/bin:/home/dbaldree/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/dbaldree/Desktop/ZClaymore
 if pgrep "miner" > /dev/null
 then
-echo $(date -u) "miner Running, continuing no change....." >> /tmp/miner.log
+logger -t MINER_HEALTHCHECKER - $(date -u) miner Running, continuing no change.....
 else
-echo $(date -u) "miner is STOPPED. Restarting......" >> /tmp/miner.log
+logger -t MINER_HEALTHCHECKER - $(date -u) miner is STOPPED. Restarting......
 tmux kill-window -t mining
 sleep 3
 tmux new-session -d -n mining
 tmux send-keys -t mining "cd /home/dbaldree/Desktop/ZClaymore" C-m
 tmux send-keys -t mining "./miner" C-m
 fi
+
