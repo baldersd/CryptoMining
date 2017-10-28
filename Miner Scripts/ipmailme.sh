f0653679eed401c78ecf45ef83883ed5f6b806ea
baldersd <danb@swiftdev.net>
2017-10-28 04:25:20 +0100

    Update ipmailme.sh

--- Miner Scripts/ipmailme.sh
+++ Miner Scripts/ipmailme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TO="danb@swiftdev.net"
-FROM="miner01@kinesysgroup.co.uk"
+TO="baldersd@hotmail.com"
+FROM="miner01@localhost"
 EXT_IP_FILE="/etc/ipaddress"
 CURRENT_IP=$(curl http://ifconfig.me/ip)
 
