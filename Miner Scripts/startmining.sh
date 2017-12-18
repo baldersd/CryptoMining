63eb68c6cc6adc43c9c6da85fa78be837034f5a3
baldersd <danb@swiftdev.net>
2017-12-18 10:16:18 +0000

    Update startmining.sh

--- Miner Scripts/startmining.sh
+++ Miner Scripts/startmining.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
-PATH=/home/dbaldree/bin:/home/dbaldree/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/dbaldree/Desktop/ZClaymore
-if pgrep "miner" > /dev/null
-then
-logger -t MINER_HEALTHCHECKER - $(date -u) miner Running, continuing no change.....
-else
-logger -t MINER_HEALTHCHECKER - $(date -u) miner is STOPPED. Restarting......
-tmux kill-window -t mining
-sleep 3
-tmux new-session -d -n mining
-tmux send-keys -t mining "cd /home/dbaldree/Desktop/ZClaymore" C-m
-tmux send-keys -t mining "./miner" C-m
+PATH=/home/dbaldree/bin:/home/dbaldree/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/dbaldree/Desktop/ZClaymore/:/home/dbaldree/Desktop/Mona/ccminer
+minerprog=`cat /home/dbaldree/Desktop/ZClaymore/whatminer`;
+mineralg=`cat /home/dbaldree/Desktop/ZClaymore/whatalg`;
+if [[ $minerprog == "miner" ]];then
+        echo 1
+        if (pgrep "miner") > /dev/null
+        then
+                logger -t MINER_HEALTHCHECKER - $(date -u) mining software Running, continuing no change.....
+        else
+                if (pgrep "tmux") > /dev/null
+                then
+                kill $(ps aux | grep '[t]mux' | awk '{print $2}')
+                fi
+                logger -t MINER_HEALTHCHECKER - $(date -u) ERROR! issue found with mining software!
+                logger -t MINER_HEALTHCHECKER - $(date -u) ^> mining software has STOPPED. Restarting......
+                tmux new-session -d -n mining
+                tmux send-keys -t mining "cd /home/dbaldree/Desktop/ZClaymore" C-m
+                cp 0.3.4b/miner miner
+                tmux send-keys -t mining "./miner" C-m
+        fi
 fi
 
+if [[ $minerprog == "ccminer" ]];then
+        echo 2
+        if (pgrep "ccminer") > /dev/null
+        then
+                logger -t MINER_HEALTHCHECKER - $(date -u) mining software Running, continuing no change.....
+        else
+                if (pgrep "tmux") > /dev/null
+                then
+                kill $(ps aux | grep '[t]mux' | awk '{print $2}')
+                fi
+                logger -t MINER_HEALTHCHECKER - $(date -u) ERROR! issue found with mining software!
+                logger -t MINER_HEALTHCHECKER - $(date -u) ^> mining software has STOPPED. Restarting......
+                sleep 3
+                if [[ $mineralg == "SIB" ]];then
+                        /home/dbaldree/Desktop/ZClaymore/overclock_enabler_SIB.sh
+                sleep 5
+                        tmux new-session -d -n mining
+                        tmux send-keys -t mining "cd /home/dbaldree/Desktop/SIB/ccminer" C-m
+                        tmux send-keys -t mining "./ccminer | tee /home/dbaldree/Desktop/ZClaymore/ccminer.log" C-m
+                elif [[ $mineralg == "MONA" ]];then
+                        /home/dbaldree/Desktop/ZClaymore/overclock_enabler.sh
+                sleep 5
+                        tmux new-session -d -n mining
+                        tmux send-keys -t mining "cd /home/dbaldree/Desktop/Mona/ccminer" C-m
+                        tmux send-keys -t mining "./ccminer | tee /home/dbaldree/Desktop/ZClaymore/ccminer.log" C-m
+                else
+                        /home/dbaldree/Desktop/ZClaymore/overclock_enabler.sh
+                sleep 5
+                        tmux new-session -d -n mining
+                        tmux send-keys -t mining "cd /home/dbaldree/Desktop/SIB/ccminer" C-m
+                        tmux send-keys -t mining "./ccminer | tee /home/dbaldree/Desktop/ZClaymore/ccminer.log" C-m
+                fi
+        fi
+fi
+exit
