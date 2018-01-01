1c3ab4f900ec41f8b8afb2a6ad6b59b7fd4df4b0
baldersd <danb@swiftdev.net>
2018-01-01 11:21:48 +0000

    Update overclock_SIB.sh

--- Miner Scripts/overclock_SIB.sh
+++ Miner Scripts/overclock_SIB.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-#just doing fans for X11 as my stock clocks are optimal.
+#!/bin/sh
 nvidia-smi -pm 1
 nvidia-smi -i 0 -pl 70
 nvidia-smi -i 1 -pl 110
@@ -19,9 +18,8 @@ nvidia-settings -c :0 -a '[gpu:3]/GPUMemoryTransferRateOffset[2]=0'
 nvidia-settings -c :0 -a '[gpu:1]/GPUGraphicsClockOffset[2]=0'
 nvidia-settings -c :0 -a '[gpu:3]/GPUGraphicsClockOffset[2]=0'
 nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=80 -c :0.0
-nvidia-settings -a [gpu:3]/GPUFanControlState=1 -a [fan:3]/GPUTargetFanSpeed=85 -c :0.0
+nvidia-settings -a [gpu:3]/GPUFanControlState=1 -a [fan:3]/GPUTargetFanSpeed=87 -c :0.0
 #<gtx 1080
 nvidia-settings -c :0 -a '[gpu:4]/GPUMemoryTransferRateOffset[2]=0'
 nvidia-settings -c :0 -a '[gpu:4]/GPUGraphicsClockOffset[2]=0'
 nvidia-settings -a [gpu:4]/GPUFanControlState=1 -a [fan:4]/GPUTargetFanSpeed=65 -c :0.0
-
